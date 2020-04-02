f57ce3b2df6698868be0d15950517f10a7a045ae
mohammed osama <mohammedosama@ieee.org>
2020-04-02 09:15:51 +0200

    push functional builder

---
+++ builder/functional-builder/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type Person struct {
+    name, position string
+}
+type personMod func(*Person)
+type PersonBuilder struct {
+    actions []personMod
+}
+
+func (b *PersonBuilder) Called(name string) *PersonBuilder {
+    b.actions = append(b.actions, func(p *Person) {
+        p.name = name
+    })
+    return b
+}
+func (b *PersonBuilder) WorksAsA(position string) *PersonBuilder {
+    b.actions = append(b.actions, func(p *Person) {
+        p.position = position
+    })
+    return b
+}
+func (b *PersonBuilder) Build() *Person {
+    p := Person{}
+    for _, action := range b.actions {
+        action(&p)
+    }
+    return &p
+}
+func main() {
+    b := PersonBuilder{}
+    p := b.Called("Thanos").WorksAsA("developer").Build()
+    fmt.Println(*p)
+}
