0a337a5f20e629e1df8b0a4ccbe47c0e96fdee93
mohammed osama <mohammedosama@ieee.org>
2020-04-08 14:44:51 +0200

    composing decorator

--- decorator/main.go
+++ decorator/main.go
@@ -28,9 +28,24 @@ type ColoredShape struct {
 func (c *ColoredShape) Render() string {
     return fmt.Sprintf("%s and with color of %s", c.Shape.Render(), c.Color)
 }
+
+type TransparentShape struct {
+    Shape        Shape
+    Transparency float32
+}
+
+func (t *TransparentShape) Render() string {
+    return fmt.Sprintf("%s has %f%% transparency", t.Shape.Render(), t.Transparency*100.0)
+}
 func main() {
     circle := Circle{11}
     fmt.Println(circle.Render())
     coloredCircle := ColoredShape{&circle, "Red"}
     fmt.Println(coloredCircle.Render())
+    // we lost the ability of doing this.
+    // coloredCircle.Resize()
+    // as this is only available for the circle, but isn't available for square.
+    // and to avoid this issue, we can compose decorators.
+    rhsCircle := TransparentShape{&coloredCircle, 0.5}
+    fmt.Println(rhsCircle.Render())
 }
